98893a593d9bd871f9f078022153b1eb673408a5
acanalesg <arturocanales@gmail.com>
2017-03-24 08:07:14 +0100

    Include config file in producer

--- producer/generate_events.py
+++ producer/generate_events.py
@@ -4,22 +4,30 @@ import csv
 from datetime import datetime
 import time
 import json
+import ConfigParser
 
 
 # Each second, we simulate are this number of seconds
-time_step = 600
 
 import logging
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)-6s| %(message)s')
 
+Config = ConfigParser.ConfigParser()
+Config.read('producer.cfg')
+
+topic = Config.get('kafka', 'topic')
+bootstrap_server = Config.get('kafka', 'server')
+data_path = Config.get('data', 'path')
+time_step = Config.getint('config', 'time_step')
+
 
 logging.info("Starting producer")
-producer = KafkaProducer(bootstrap_servers='localhost:9092',
+producer = KafkaProducer(bootstrap_servers=bootstrap_server,
                          value_serializer=lambda m: json.dumps(m).encode('ascii'))
 
 
-with open('/Users/asp/Develop/equinox_kafka/Mexico_ordenado.csv', 'r') as f:
+with open(data_path, 'r') as f:
     r = csv.DictReader(f)
     line = r.next()
     # longitude, datetime, iddevice, latitude, speed, id
@@ -40,5 +48,5 @@ with open('/Users/asp/Develop/equinox_kafka/Mexico_ordenado.csv', 'r') as f:
         time.sleep((event_dt - current_dt).seconds/time_step)
 
         logging.info("Generating event: " + str(line))
-        ev = producer.send('positionupdates', line)
+        ev = producer.send(topic, line)
         current_dt = event_dt
